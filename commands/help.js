f188ea60caf571a7bc8162eacd14803d249fd0c6
Eries Trisnadi <zdumb1885@outlook.com>
2018-08-16 18:15:41 +0700

    hotfix

--- commands/help.js
+++ commands/help.js
@@ -5,12 +5,12 @@ module.exports = function (message) {
   const embeded = new RichEmbed()
     .setColor(color)
     .setTitle('Perintah yang didukung oleh bot ini:')
-    .addField(`${symbol}help`, 'Menampilkan halaman ini.')
-    .addField(`${symbol}play <nama lagu/penyanyi>`, `Memutar lagu berdasarkan kata kunci yang diberikan.`)
-    .addField(`${symbol}search <nama lagu/penyanyi>`, `Mencari lagu dan memberikan kamu pilihan lagu mana yang akan ditambahkan ke dalam daftar pemutar.`)
-    .addField(`${symbol}skip`, 'Melewati lagu yang sekarang sedang diputar.')
-    .addField(`${symbol}queue`, 'Menampilkan daftar lagu yang akan diputar.')
-    .addField(`${symbol}leave`, 'Memerintahkan bot untuk keluar dari voice channel.');
+    .addField(`${symbol} help`, 'Menampilkan halaman ini.')
+    .addField(`${symbol} play <nama lagu/penyanyi>`, `Memutar lagu berdasarkan kata kunci yang diberikan.`)
+    .addField(`${symbol} search <nama lagu/penyanyi>`, `Mencari lagu dan memberikan kamu pilihan lagu mana yang akan ditambahkan ke dalam daftar pemutar.`)
+    .addField(`${symbol} skip`, 'Melewati lagu yang sekarang sedang diputar.')
+    .addField(`${symbol} queue`, 'Menampilkan daftar lagu yang akan diputar.')
+    .addField(`${symbol} leave`, 'Memerintahkan bot untuk keluar dari voice channel.');
 
   return message.channel.send(embeded);
 }
