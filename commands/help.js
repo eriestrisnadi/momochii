fa2bb0c77867911d77c4e07cf76bdb0479a0fbc0
Eries Trisnadi <zdumb1885@outlook.com>
2018-08-16 17:06:51 +0700

    add help description for search command

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,8 @@ module.exports = function (message) {
     .setColor(color)
     .setTitle('Perintah yang didukung oleh bot ini:')
     .addField(`${symbol}help`, 'Menampilkan halaman ini.')
-    .addField(`${symbol}play <nama lagu/penyanyi>`, `Memutar / mencari lagu.`)
+    .addField(`${symbol}play <nama lagu/penyanyi>`, `Memutar lagu berdasarkan kata kunci yang diberikan.`)
+    .addField(`${symbol}search <nama lagu/penyanyi>`, `Mencari lagu dan memberikan kamu pilihan lagu mana yang akan ditambahkan ke dalam daftar pemutar.`)
     .addField(`${symbol}skip`, 'Melewati lagu yang sekarang sedang diputar.')
     .addField(`${symbol}queue`, 'Menampilkan daftar lagu yang akan diputar.')
     .addField(`${symbol}leave`, 'Memerintahkan bot untuk keluar dari voice channel.');
